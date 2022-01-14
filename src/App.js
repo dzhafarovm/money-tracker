aa65e1e13257e25845314a9bf2affb1036b07492
AlexRosly <rosly.a777@gmail.com>
2022-01-14 05:57:58 +0200

    Added svg-sprite

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import sprite from '../src/images/sprite.svg';
+
 function App() {
-  return <h1>Hello</h1>;
+  return (
+    <>
+      <h1>Hello</h1>
+      {/* Пример подключения и использования спрайта */}
+      <svg>
+        <use href={`${sprite}#main-salary`}></use>
+      </svg>
+    </>
+  );
 }
 
 export default App;
