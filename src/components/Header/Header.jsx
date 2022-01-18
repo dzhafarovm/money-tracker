db306648338b717941aa60c30d8e8fda7ab2ee50
Maksym Dzhafarov <maksim8797@gmail.com>
2022-01-18 23:22:22 +0200

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,20 @@
+import { useSelector } from 'react-redux';
+import authSelectors from 'redux/auth/auth-selectors';
 import UserMenu from './UserMenu';
 
 import style from './Header.module.css';
 import sprite from 'components/images/sprite.svg';
 
 const Header = () => {
+  const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
+
   return (
     <header className={style.header}>
       <div className={style.navConteiner}>
         <svg width="90" height="31">
           <use href={`${sprite}#logo`}></use>
         </svg>
-
-        <UserMenu />
-       
+        {isLoggedIn ? <UserMenu /> : null}
       </div>
     </header>
   );
