a51a127595e6f97fd0a650ff85bda7f8f2f7ac4a
Andrey Bulanov <bulanov1804@gmail.com>
2022-01-19 14:14:11 +0200

    4 section report

---
+++ src/components/NavigationReport/NavigationReport.jsx
@@ -0,0 +1,36 @@
+import { useNavigate } from 'react-router-dom';
+import style from './NavigationReport.module.css';
+import sprite from '../../images/sprite.svg';
+
+const NavigationReport = () => {
+    const navigate = useNavigate();
+
+    return (
+        <nav className={style.nav}>  
+            
+            <button onClick={() => navigate('/home')} className={style.button} type="button">
+                <svg width="18" height="12" viewBox="0 0 24 24" className={style.icon}>
+                    <use href={`${sprite}#arrow`}></use>  
+                </svg>
+                <p className={style.title}>Вернуться на главную</p>
+            </button>
+            
+            {/* <p className={style.title}>Баланс:</p> */}
+            
+            <span>
+                <p className={style.currentDate}>Текущий период:</p>
+                <span className={style.slider}>
+                    <svg width="12" height="18" viewBox="0 0 24 24" className={style.iconDate}>
+                        <use href={`${sprite}#arrow-left`}></use>  
+                    </svg>
+                    <p className={style.date}>НОЯБРЬ 2019</p>
+                    <svg width="12" height="18" viewBox="0 0 24 24" className={style.iconDate}>
+                        <use href={`${sprite}#arrow-right`}></use>  
+                    </svg>
+                </span>
+            </span>
+        </nav>
+    )
+};
+
+export default NavigationReport;
