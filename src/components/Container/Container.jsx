ca75e19aa59dae50db5ff3cf61987e8325f654be
Maksym Dzhafarov <maksim8797@gmail.com>
2022-01-21 12:17:10 +0200

    fix bcg

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,23 +1,22 @@
-// import { useLocation } from 'react-router-dom';
-import style from './Container.module.css';
+import { useLocation } from 'react-router-dom';
 
+import routes from 'routes/routes';
+
+import style from './Container.module.css';
 import stylesBg from 'components/StylesBg/StylesBg.module.css';
 
 const Container = ({ children }) => {
-  // const location = useLocation();
-  
-  // let pageAuth = location.pathname === '/authorization';
-  // let bg = pageAuth ? 'mainBgAuth' : 'mainBg'; /**потом поставить переменую  bg вместо имени класса '.mainBgAuth'*/
-  
+  const { pathname } = useLocation();
+
   return (
-    
-    <div className={stylesBg.mainBgAuth}>
-      <div className={style.container}>
-        {children}
-      </div>
+    <div
+      className={
+        pathname === routes.auth ? `${stylesBg.mainBgAuth} ${stylesBg.greyFon}` : stylesBg.mainBg
+      }
+    >
+      <div className={style.container}>{children}</div>
     </div>
-  
-  )
+  );
 };
 
 export default Container;
