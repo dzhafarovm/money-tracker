79db4f3b0444006df7cf660be220e44eabf8e300
AlexRosly <rosly.a777@gmail.com>
2022-01-23 15:45:07 +0200

    Added calendar and validate form

--- src/components/NavigationReport/NavigationReport.jsx
+++ src/components/NavigationReport/NavigationReport.jsx
@@ -1,9 +1,49 @@
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+
 import style from './NavigationReport.module.css';
 import sprite from 'components/images/sprite.svg';
 
+const arrMonthName = [
+  { id: '1', name: 'январь' },
+  { id: '2', name: 'февраль' },
+  { id: '3', name: 'март' },
+  { id: '4', name: 'апрель' },
+  { id: '5', name: 'май' },
+  { id: '6', name: 'июнь' },
+  { id: '7', name: 'июль' },
+  { id: '8', name: 'август' },
+  { id: '9', name: 'сентябрь' },
+  { id: '10', name: 'октябрь' },
+  { id: '11', name: 'ноябрь' },
+  { id: '12', name: 'декабрь' },
+];
+
 const NavigationReport = () => {
   const navigate = useNavigate();
+
+  const nowDate = new Date();
+  const [mounth, setMounth] = useState(nowDate.getMonth() + 1);
+  const [year, setYear] = useState(nowDate.getFullYear());
+
+  const handlePrevMounth = () => {
+    if (mounth <= 1) {
+      setMounth(12);
+      setYear(prev => (prev -= 1));
+    } else {
+      setMounth(prev => (prev -= 1));
+    }
+  };
+  const handleNextMounth = () => {
+    if (mounth < 12) {
+      setMounth(prev => (prev += 1));
+    } else {
+      setMounth(1);
+      setYear(prev => (prev += 1));
+    }
+  };
+
+  const getMounth = arrMonthName.filter(el => el.id === String(mounth));
 
   return (
     <nav className={style.nav}>
@@ -23,29 +63,40 @@ const NavigationReport = () => {
       <span>
         <p className={style.currentDate}>Текущий период:</p>
         <span className={style.slider}>
-          <button type="button" className={style.arrowDate}>
-            <svg
-            width="12"
-            height="18"
-            viewBox="0 0 24 24"
-            className={style.iconDate}
+          <button
+            type="button"
+            className={style.arrowDate}
+            onClick={handlePrevMounth}
           >
-            <use href={`${sprite}#arrow-left`}></use>
-          </svg>
+            <svg
+              width="12"
+              height="18"
+              viewBox="0 0 24 24"
+              className={style.iconDate}
+            >
+              <use href={`${sprite}#arrow-left`}></use>
+            </svg>
           </button>
-          
-          <p className={style.date}>НОЯБРЬ 2019</p>
 
-          <button type="button" className={style.arrowDate}>
-            <svg
-            width="12"
-            height="18"
-            viewBox="0 0 24 24"
-            className={style.iconDate}
+          <p className={style.date}>
+            {getMounth[0].name.toLocaleUpperCase()}
+            <span className={style.year}>{year}</span>
+          </p>
+
+          <button
+            type="button"
+            className={style.arrowDate}
+            onClick={handleNextMounth}
           >
-            <use href={`${sprite}#arrow-right`}></use>
-          </svg>
-          </button>       
+            <svg
+              width="12"
+              height="18"
+              viewBox="0 0 24 24"
+              className={style.iconDate}
+            >
+              <use href={`${sprite}#arrow-right`}></use>
+            </svg>
+          </button>
         </span>
       </span>
     </nav>
