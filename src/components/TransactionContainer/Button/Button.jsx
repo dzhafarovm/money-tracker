3f7566453b9512c5af19837b7dddc10ede90dc4d
Maria-Tkh <mcweaver117@gmail.com>
2022-01-26 02:11:05 +0200

    Update Button.jsx

--- src/components/TransactionContainer/Button/Button.jsx
+++ src/components/TransactionContainer/Button/Button.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import style from './Button.module.css';
+// import style from './Button.module.css';
 
-const Button = ({ title, route}) => {
+const Button = ({ title, route, style }) => {
   return (
     <NavLink
       to={route}
-      className={({isActive}) => `${isActive ? style.active : style.inactive}`
-    }
-    > {title}            
+      className={({ isActive }) =>
+        isActive ? 'style.active' : 'style.inactive'
+      }
+    >
+      {' '}
+      {title}
     </NavLink>
   );
-}
+};
 
 export default Button;
