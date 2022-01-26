6af7b494614b82b8632693cc21d68be973d2529a
Maria-Tkh <mcweaver117@gmail.com>
2022-01-26 14:40:36 +0200

    jsx

--- src/pages/ExpensesPage/index.js
+++ src/pages/ExpensesPage/index.js
@@ -1,1 +1,1 @@
-export { default } from './ExpensesPage.jsx';
+export { default } from './ExpensesPage';
--- src/pages/IncomePage/index.js
+++ src/pages/IncomePage/index.js
@@ -1,1 +1,1 @@
-export { default } from './IncomePage.jsx'
+export { default } from './IncomePage'
